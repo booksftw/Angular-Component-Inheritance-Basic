1c85652caa4d5040a97a89a9f2825a5a8d38e77b
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-02-25 16:19:02 -0800

    quick button styling with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseComponent } from './base/base.component';
 import { PageoneComponent } from './base/pageone/pageone.component';
 import { PagetwoComponent } from './base/pagetwo/pagetwo.component';
 import { PagethreeComponent } from './base/pagethree/pagethree.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { PagethreeComponent } from './base/pagethree/pagethree.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
